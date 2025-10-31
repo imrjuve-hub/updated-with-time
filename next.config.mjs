64c7142f0a53787ea6b5d72b97461c9bf0ee201e
imrjuve-hub <imr.juve@gmail.com>
2025-10-31 14:10:18 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'ddragon.leagueoflegends.com' },
-    ],
-  },
-};
-export default nextConfig;
